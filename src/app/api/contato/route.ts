4a1011e2c485e1196ef5416bd92967616200a172
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-03-12 16:08:27 -0300

    feat/build

--- src/app/api/contato/route.ts
+++ src/app/api/contato/route.ts
@@ -1,9 +1,8 @@
 // defaults to auto
 import { PrismaClient } from '@prisma/client'
-import FormData from 'form-data'
-import Mailgun from 'mailgun.js'
-export const dynamic = 'force-dynamic'
 
+import { Resend } from 'resend'
+export const dynamic = 'force-dynamic'
 const prisma = new PrismaClient()
 export async function POST(req: Request) {
   const response = await req.json()
@@ -39,67 +38,20 @@ export async function POST(req: Request) {
       },
     })
 
-    const mailgun = new Mailgun(FormData)
-    const mg = mailgun.client({
-      username: 'api',
-      key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere',
+    const resend = new Resend(process.env.RESEND_API_KEY)
+
+    await resend.emails.send({
+      from: 'Code labz <contato@codelabz.com.br>',
+      to: 'viniciusc.d.c@hotmail.com',
+      subject: `Proposta de ${name}`,
+      html: `<div>
+           <h1>Proposta de ${name}</h1>
+           <p>Email: ${email}</p>
+           <p>Empresa: ${company}</p>
+           <p>Telefone: ${phone}</p>
+           <p>${description}</p>
+         </div>`,
     })
-
-    mg.messages
-      .create('codelabz.com.br', {
-        from: 'Codelabz <contato@codelabz.com.br>',
-        to: ['viniciusc.d.c@hotmail.com'],
-        subject: `Proposta de ${name}`,
-        html: `<div>
-        <h1>Proposta de ${name}</h1>
-        <p>Email: ${email}</p>
-        <p>Empresa: ${company}</p>
-        <p>Telefone: ${phone}</p>
-        <p>${description}</p>
-      </div>`,
-      })
-      .then((msg) => console.log(msg)) // logs response data
-      .catch((err) => {
-        console.log(err)
-        return Response.json(
-          { error: 'Erro ao enviar email', description: err },
-          {
-            status: 400,
-            headers: {
-              'content-type': 'application/json',
-            },
-          },
-        )
-      }) // logs any error
-    // mg.messages
-    //   .create('sandbox-123.mailgun.org', {
-    //     from: 'Codelabz <contato@codelabz.com.br>',
-    //     to: [email],
-    //     subject: `Proposta de ${name}`,
-    //     html: `<div>
-    //     <h1>Proposta de ${name}</h1>
-    //     <p>Email: ${email}</p>
-    //     <p>Empresa: ${company}</p>
-    //     <p>Telefone: ${phone}</p>
-    //     <p>${description}</p>
-    //   </div>`,
-    //   })
-    //   .then((msg) => console.log(msg)) // logs response data
-    //   .catch((err) => console.log(err)) // logs any error
-
-    // mg.messages().send(
-    //   {
-    //     from: 'Mailgun Sandbox <postmaster@sandboxb119293e63f14ec8979f8825aa51ecef.mailgun.org>',
-    //     to: email,
-    //     subject: `Proposta de enviada com sucesso !`,
-    //     text: `<p>Muito obrigado por entrar em contato, entraremos em contato o mais rápido possível </p>`,
-    //   },
-    //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    //   (error: any, body: any) => {
-    //     console.log(body)
-    //     console.log(error)
-    //   },
-    // )
 
     return Response.json(
       { message: 'Cliente criado com sucesso' },
