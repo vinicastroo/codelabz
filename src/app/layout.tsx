d4ae14ea58263b07fe655b95ec2b11ff3e8f9a2d
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-06-21 14:11:07 -0300

    feat/change title SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,7 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 const roboto = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
-  title:
-    'Codelabz - Criação de sites, Desenvolvimento de sistemas, criação de landing pages. Rio do sul / Santa Catarina',
+  title: 'Codelabz - Criação de sites em Rio do Sul / Santa Catarina',
   keywords: [
     'Criação de sites',
     'Desenvolvimento de sistemas',
@@ -28,8 +27,7 @@ export const metadata: Metadata = {
   description:
     'Transforme suas ideias em realidade com nossas soluções digitais abrangentes. Vamos além da criação de sites, oferecendo estratégias digitais que impulsionam o sucesso online do seu negócio. Descubra como podemos posicionar sua marca de forma eficaz no mundo digital e gerar resultados reais para o seu negócio.',
   openGraph: {
-    title:
-      'Codelabz - Criação de sites, Desenvolvimento de sistemas, criação de landing pages. Rio do sul / Santa Catarina',
+    title: 'Codelabz - Criação de sites em Rio do Sul / Santa Catarina',
     description:
       'Transforme suas ideias em realidade com nossas soluções digitais abrangentes. Vamos além da criação de sites ou desenvolvimento de sistemas, oferecendo estratégias digitais que impulsionam o sucesso online do seu negócio. Descubra como podemos posicionar sua marca de forma eficaz no mundo digital e gerar resultados reais para o seu negócio.',
     url: 'https://www.codelabz.com.br/',
