41897bdbe28afaef5d19352b6c00fad7edb450e2
Vinicius Castro <viniciusc.d.c@hotmail.com>
2024-10-14 15:29:32 -0300

    feat/add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,11 @@ import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Suspense } from 'react'
-import { FacebookPixelEvents } from './Components/FacebookPixelEvents'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 const roboto = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
   title: 'Codelabz - Criação de sites e Desenvolvimento de sistemas',
   keywords: [
@@ -77,10 +78,32 @@ export default function RootLayout({
         </div>
 
         <Suspense fallback={null}>
-          <FacebookPixelEvents />
-
           <GoogleAnalytics gaId="G-ZMSWXB8HFV" />
         </Suspense>
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '2243382096030650');
+          fbq('track', 'PageView');
+        `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{
+              display: 'none',
+            }}
+            src="https://www.facebook.com/tr?id=2243382096030650&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   )
