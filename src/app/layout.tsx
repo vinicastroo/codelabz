9b5840952733fd048955c283a79106daf313ecb1
Vinicius Castro <viniciusc.d.c@hotmail.com>
2025-04-16 03:18:46 -0300

    feat/add horjar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,7 @@ export default function RootLayout({
           content="10ffa802-5b28-4ed5-b6f0-652a2bb04ca8"
         />
       </Head>
+
       <body className={roboto.className}>
         <div className="min-h-screen">
           {/* <Menu /> */}
@@ -111,6 +112,18 @@ export default function RootLayout({
           src="//tag.goadopt.io/injector.js?website_code=10ffa802-5b28-4ed5-b6f0-652a2bb04ca8"
           className="adopt-injector"
         ></Script>
+        <Script id="hotjar" strategy="afterInteractive">
+          {`
+            (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:6373518,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}
+        </Script>
         <noscript>
           <img
             height="1"
